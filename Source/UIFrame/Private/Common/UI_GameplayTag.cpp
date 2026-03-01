2129b69df730ab9391af6778e43c468c54021e3f
LLwifi <1204717710@qq.com>
2026-03-01 22:01:52 +0800

    2026.3.1-添加SlotUITag避免新添加进插件时的报错

--- Source/UIFrame/Private/Common/UI_GameplayTag.cpp
+++ Source/UIFrame/Private/Common/UI_GameplayTag.cpp
@@ -3,4 +3,13 @@
 
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_TipType_Common, "TipType.Common", "通用提示类型");
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_TipType_Queue, "TipType.Queue", "队列提示类型");
-UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_TipType_SingleOverride, "TipType.SingleOverride", "单一提示覆盖类型")
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_TipType_SingleOverride, "TipType.SingleOverride", "单一提示覆盖类型");
+
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_Root, "UI.Layout.Root", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_SeparatelyShow, "UI.Layout.SeparatelyShow", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_PanelShow, "UI.Layout.PanelShow", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_Bot, "UI.Layout.Bot", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_Mid, "UI.Layout.Mid", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_Top, "UI.Layout.Top", "UITag");
+UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_UI_Layout_System, "UI.Layout.System", "UITag");
+
